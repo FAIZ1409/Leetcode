2a1efe64c2080265fc2d3a7c86c461c4a022e4be
Mohammed Faiz Y <faizacct@gmail.com>
2025-02-18 21:54:43 +0530

    Create 67) Primary Department for Each Employee.sql

---
+++ 67) Primary Department for Each Employee.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+SELECT employee_id,department_id from Employee where primary_flag='Y' or employee_id in (select employee_id from Employee Group by employee_id having count(employee_id)=1) 
